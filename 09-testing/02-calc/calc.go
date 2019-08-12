8216b68c412b8a674626c69a15bbef3d9cb9b852
rafaelpardor <rafaelpardor@protonmail.com>
2019-08-11 20:43:18 -0500

    09 Testing

---
+++ 09-testing/02-calc/calc.go
@@ -0,0 +1,17 @@
+package calc
+
+func Add(x, y int) int {
+	return x + y
+}
+
+func Subs(x, y int) int {
+	return x - y
+}
+
+func Mult(x, y int) int {
+	return x * y
+}
+
+func Div(x, y int) int {
+	return x / y
+}
