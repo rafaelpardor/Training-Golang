739a3a6220347ff77314a9e8f855c6df5f1aca5e
rafaelpardor <rafaelpardor@protonmail.com>
2022-01-10 18:06:53 -0500

    Go Fundamentals
    Working with boolean data type

--- go-fundamentals/02-types/03-boolean_expressions/01-true_false/main.go
+++ go-fundamentals/02-types/03-boolean_expressions/01-true_false/main.go
@@ -3,11 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("Booleans are a type of data that Go supports")
 	if true {
 		fmt.Println("This code will ran.")
 	}
 
 	if false {
-		fmt.Println("This did not ruun.")
+		fmt.Println("This did not run.")
 	}
 }
