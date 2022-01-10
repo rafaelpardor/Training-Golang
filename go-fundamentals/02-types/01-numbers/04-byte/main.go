e749820a0e3e57168a77b553057a4fad1ab6ad38
rafaelpardor <rafaelpardor@protonmail.com>
2022-01-10 18:48:53 -0500

    Go Fundamentals
    Updating chapter 2 of go fundamaentals

--- go-fundamentals/02-types/01-numbers/04-byte/main.go
+++ go-fundamentals/02-types/01-numbers/04-byte/main.go
@@ -5,9 +5,10 @@ import (
 )
 
 func main() {
-	fmt.Println("Byte is just an alias of uint8")
+	fmt.Println("Byte is just an alias of uint8, this meaning that it will only receive positive numbers.")
 
 	var x byte = 10
+	//	var err byte = -10
 	byteArr := []byte{1, 2, 3}
 
 	fmt.Printf("The variale x is a '<%T>' with a value of %v.\n", x, x)
