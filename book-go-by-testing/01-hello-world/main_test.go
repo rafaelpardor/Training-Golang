6357d0b937a2ba7ed59a32e5a7a1148dbfc6ecd4
rafaelpardor <rafaelpardor@protonmail.com>
2022-03-24 14:16:18 -0500

    Adding test exception, if function 'Hello', does not have argument

--- book-go-by-testing/01-hello-world/main_test.go
+++ book-go-by-testing/01-hello-world/main_test.go
@@ -2,10 +2,21 @@ package main
 
 import "testing"
 
-func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+func TestHelloFunction(t *testing.T) {
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
 	}
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Rafael")
+		want := "Hello, Rafael"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
 }
